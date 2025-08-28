74d3d239304b8285a8dad8805e24e7ad961c5df8
koigor97 <turaysamuel101@yahoo.com>
2025-08-28 19:29:14 +0200

    refactor test code using testing package and map

--- hello-go/main_internal_test.go
+++ hello-go/main_internal_test.go
@@ -3,12 +3,42 @@ package main
 import "testing"
 
 func TestGreetingWorld(t *testing.T) {
-	want := "Hello world"
+	type testCase struct {
+		lang language
+		want string
+	}
 
-	got := greetingsWorld()
+	var tests = map[string]testCase{
+		"English": {
+			lang: "en",
+			want: "Hello world",
+		},
+		"French": {
+			lang: "fr",
+			want: "Bonjour le monde",
+		},
+		"Akkadian, not supported": {
+			lang: "akk",
+			want: `unsupported language: "akk"`,
+		},
+		"Greek": {
+			lang: "el",
+			want: "Χαίρετε Κόσμε",
+		},
+		"Empty": {
+			lang: "",
+			want: `unsupported language: ""`,
+		},
+	}
 
-	if got != want {
-		// mark as test failed
-		t.Errorf("expected: %q. got: %q", want, got)
+	// range over all the scenarios
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			got := greetingsWorld(tc.lang)
+
+			if got != tc.want {
+				t.Errorf("expected: %v, got: %v", tc.want, got)
+			}
+		})
 	}
 }
