c462e4d212ec0182c5c82f7d023279fd51dd2432
koigor97 <turaysamuel101@yahoo.com>
2025-09-04 23:02:03 +0200

    comment previous functions

--- go-fundamentals/main.go
+++ go-fundamentals/main.go
@@ -4,7 +4,8 @@ import "fmt"
 
 func main() {
 	fmt.Println("           GoLang Fundamentals...        ")
-	variableFuncWithGreeting("Samuel Turay", "Sierra Leone")
-	userId := userInputFunction()
-	fmt.Println(userId)
+	//variableFuncWithGreeting("Samuel Turay", "Sierra Leone")
+	//userId := userInputFunction()
+	//fmt.Println(userId)
+
 }
