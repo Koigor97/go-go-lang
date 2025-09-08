8058c2c1c892c277b61bfb95f4d7d66bcd926b8b
koigor97 <turaysamuel101@yahoo.com>
2025-09-08 23:30:39 +0200

    add more test case

--- bookworm-digest/bookworms_internal_test.go
+++ bookworm-digest/bookworms_internal_test.go
@@ -56,3 +56,65 @@ func TestLoadBookworms_Success(t *testing.T) {
 	}
 
 }
+
+// equalBooksCount is a helper to test the equality of the two maps
+// of books count.
+func equalBooksCount(t *testing.T, got, want map[Book]uint) bool {
+	t.Helper()
+
+	if len(got) != len(want) {
+		return false
+	}
+
+	for book, targetCount := range want {
+		gotCount, ok := got[book]
+		if !ok || targetCount != gotCount {
+			return false
+		}
+	}
+
+	return true
+}
+
+func TestBookCount(t *testing.T) {
+	tt := map[string]struct {
+		input []Bookworm
+		want  map[Book]uint
+	}{
+		"nominal use case": {
+			input: []Bookworm{
+				{Name: "Fadi", Books: []Book{
+					handmaidsTale, theBellJar,
+				}},
+				{Name: "Peggy", Books: []Book{
+					oryxAndCrake, handmaidsTale, janeEyre,
+				}},
+			},
+			want: map[Book]uint{
+				handmaidsTale: 2,
+				theBellJar:    1,
+				oryxAndCrake:  1,
+				janeEyre:      1,
+			},
+		},
+		"no bookworms": {
+			input: []Bookworm{},
+			want:  map[Book]uint{},
+		},
+		"bookworm without books": {
+			input: []Bookworm{},
+			want:  map[Book]uint{},
+		},
+	}
+
+	for name, testCase := range tt {
+		t.Run(name, func(t *testing.T) {
+			got := booksCount(testCase.input)
+			if !equalBooksCount(t, got, testCase.want) {
+				t.Fatalf(
+					"got a different list of books: %v, expected %v", got, testCase.want)
+			}
+		})
+	}
+
+}
